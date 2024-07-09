1dcb9b376c556ab3258612f94b35a823024773f5
Bappa Marjit <bappa.marjit98@gmail.com>
2024-07-09 22:29:21 +0530

    practice changes

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// const url = "https://open.er-api.com/v6/latest/USD";
-// let select = document.querySelector("select");
-// let option = document.querySelector("option");
+const url = "https://open.er-api.com/v6/latest/USD";
+let select = document.querySelector("select");
+let option = document.querySelector("option");
 
-// async function getRates(){
-// await fetch(url).then((res)=>{
-// res.json().then(function(data){
-// console.log(data);
-// let key = Object.keys(data.rates);
-// select.innerHTML = `<option>${key[0]}</option>;
-// <option>${key[1]}</option>`
+async function getRates(){
+await fetch(url).then((res)=>{
+res.json().then(function(data){
+console.log(data);
+let key = Object.keys(data.rates);
+select.innerHTML = `<option>${key[0]}</option>;
+<option>${key[1]}</option>`
 
 
-// })
-//     })
-// }
+})
+    })
+}
 
 
 // let args = process.argv;
